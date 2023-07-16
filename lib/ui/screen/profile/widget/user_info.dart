bdc8db167224dc662c2a735010f2b77e5002d9e8
jeavaljean <jeanvaljean0520@gmail.com>
2023-07-16 14:54:20 +0900

    [add] : 유저가 가장 좋아했던 여행장소 추가 + ui 간격 조절

--- lib/ui/screen/profile/widget/user_info.dart
+++ lib/ui/screen/profile/widget/user_info.dart
@@ -7,22 +7,28 @@ class UserInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         _UserProfileImage(
           imageURL: "https://picsum.photos/250?image=9",
         ),
+        SizedBox(
+          width: 30.w,
+        ),
         Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             _UserProfileDetailInfo(
               post: 1,
-              follower: 22,
+              follower: 2222,
               following: 3,
             ),
-            _UserSelfIntroduce(
-              selfIntroduce: "hello every body this is nuri officer thank you user Nuri",
-            )
+            _UserFavoriteTravel(
+              favoriteTravel: "제주도",
+            ),
+            // _UserSelfIntroduce(
+            //   selfIntroduce: "hello every body this is nuri officer thank you user Nuri",
+            // )
           ],
         )
       ],
@@ -30,68 +36,137 @@ class UserInfo extends StatelessWidget {
   }
 }
 
+@immutable
 class _UserProfileImage extends StatelessWidget {
   _UserProfileImage({super.key, this.imageURL});
+
   String? imageURL;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 120.w,
-      width: 120.w,
-      color: imageURL != null ? null : Colors.grey,
-      child: imageURL != null ? Image.network(imageURL!) : Icon(Icons.person, size: 72.sp,),
+      height: 100.w,
+      width: 100.w,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: imageURL != null ? null : Colors.grey,
+      ),
+      child: imageURL != null
+          ? ClipOval(
+              child: Image.network(
+              imageURL!,
+              fit: BoxFit.fill,
+            ))
+          : Icon(
+              Icons.person,
+              size: 72.sp,
+            ),
     );
   }
 }
 
+@immutable
 class _UserProfileDetailInfo extends StatelessWidget {
-  _UserProfileDetailInfo({super.key, required this.follower, required this.following, required this.post});
+  _UserProfileDetailInfo(
+      {super.key,
+      required this.follower,
+      required this.following,
+      required this.post});
+
   int post = 0;
   int following = 0;
   int follower = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
         height: 35.h,
         width: 200.w,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("게시글",style: TextStyle(fontSize: 12.sp)),
-                Text("팔로워",style: TextStyle(fontSize: 12.sp)),
-                Text("팔로잉",style: TextStyle(fontSize: 12.sp)),
+                SizedBox(
+                    height: 10.h,
+                    width: 40.w,
+                    child: Text("여행", style: TextStyle(fontSize: 12.sp))),
+                SizedBox(
+                    height: 10.h,
+                    width: 40.w,
+                    child: Text("팔로워", style: TextStyle(fontSize: 12.sp))),
+                SizedBox(
+                    height: 10.h,
+                    width: 40.w,
+                    child: Text("팔로잉", style: TextStyle(fontSize: 12.sp))),
               ],
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("$post",style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.bold)),
-                Text("$follower",style: TextStyle(fontSize: 12.sp, fontWeight:  FontWeight.bold)),
-                Text("$following",style: TextStyle(fontSize: 12.sp, fontWeight: FontWeight.bold)),
+                SizedBox(
+                    height: 10.h,
+                    width: 40.w,
+                    child: Text("$post",
+                        style: TextStyle(
+                            fontSize: 12.sp, fontWeight: FontWeight.bold))),
+                SizedBox(
+                    height: 10.h,
+                    width: 40.w,
+                    child: Text("$follower",
+                        style: TextStyle(
+                            fontSize: 12.sp, fontWeight: FontWeight.bold))),
+                SizedBox(
+                    height: 10.h,
+                    width: 40.w,
+                    child: Text("$following",
+                        style: TextStyle(
+                            fontSize: 12.sp, fontWeight: FontWeight.bold))),
               ],
             ),
           ],
-        )
-    );
+        ));
   }
 }
 
+@immutable
 class _UserSelfIntroduce extends StatelessWidget {
   _UserSelfIntroduce({super.key, required this.selfIntroduce});
+
   String? selfIntroduce;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: 65.h,
       width: 200.w,
-      child: Center(child: Text("$selfIntroduce",style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.bold),)),
+      child: Center(
+          child: Text(
+        "$selfIntroduce",
+        style: TextStyle(fontSize: 12.sp, fontWeight: FontWeight.bold),
+      )),
     );
   }
 }
+
+@immutable
+class _UserFavoriteTravel extends StatelessWidget {
+  _UserFavoriteTravel({super.key, this.favoriteTravel});
+
+  String? favoriteTravel;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 20.h,
+      width: 200.w,
+      child: Text(
+        "최고의 여행지 : $favoriteTravel",
+        style: TextStyle(fontSize: 12.sp, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
 
