b37ec851d8ac0639e02624982c3ae7c3b65cdd59
jeavaljean <jeanvaljean0520@gmail.com>
2023-07-20 15:57:03 +0900

    [add] : user profile api

---
+++ lib/data/remote/user_profile/user_profile_api.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:nuri/config/api.dart';
+import 'package:nuri/data/model/profile/profile_model.dart';
+
+class UserProfileApi extends Api{
+  static final UserProfileApi _instance = UserProfileApi._internal();
+  UserProfileApi._internal();
+
+  factory UserProfileApi(){
+    return _instance;
+  }
+
+  Future<ProfileModel> getUserProfileInfo() async{
+    try {
+      Response response = await dio.get('$baseUrl/');
+
+      ProfileModel getData = response.data["data"];
+
+      return getData;
+    }
+    on DioError catch (e){
+      return Future.error(e);
+    }
+  }
+
+  Future<bool> fetchUserProfile({required ProfileModel profileModel}) async{
+    try{
+      Map requestBody = {};
+
+      Response response = await dio.post('$baseUrl/', data: requestBody);
+      return true;
+    }
+    catch(e){
+      return false;
+    }
+  }
+}
