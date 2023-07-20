1154147c5d5c2a82903ee80b319f35825112ecca
jeavaljean <jeanvaljean0520@gmail.com>
2023-07-21 00:53:26 +0900

    [add] : ui 개발

--- lib/ui/screen/travel/widget/travel_post.dart
+++ lib/ui/screen/travel/widget/travel_post.dart
@@ -20,6 +20,7 @@ class _TravelPostState extends State<TravelPost> {
       itemCount: widget.travelPostModel.length,
         itemBuilder: (context, index){
           return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               _PostInfo(
                   imageURL: widget.travelPostModel[index].userInfo.userProfile,
@@ -35,7 +36,25 @@ class _TravelPostState extends State<TravelPost> {
                 ],
               ),
               Text(widget.travelPostModel[index].content),
-
+              SizedBox(height: 10.h,),
+              const Text("주요 여행지"),
+              // _ImportantTravel(travelName: widget.travelPostModel[index].mainTravel),
+              SizedBox(height: 20.h,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _Cost(cost: widget.travelPostModel[index].cost),
+                  Row(
+                    children: [
+                      _ApplyButton(),
+                      _SettingButton()
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.h,),
+              Container(height: 0.1, width: double.infinity, color: Colors.black,),
+              SizedBox(height: 20.h,),
             ],
           );
         }
@@ -57,40 +76,99 @@ class _PostInfo extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Row(
-          children: [
-            Container(
-              height: 30.w,
-              width: 30.w,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: imageURL != null ? null : Colors.grey,
+        SizedBox(
+          width: 100.w,
+          child: Row(
+            children: [
+              Container(
+                height: 30.w,
+                width: 30.w,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: imageURL != null ? null : Colors.grey,
+                ),
+                child: imageURL != null
+                    ? ClipOval(
+                    child: Image.network(
+                      imageURL!,
+                      fit: BoxFit.fill,
+                    ))
+                    : Icon(
+                  Icons.person,
+                  size: 24.sp,
+                ),
               ),
-              child: imageURL != null
-                  ? ClipOval(
-                  child: Image.network(
-                    imageURL!,
-                    fit: BoxFit.fill,
-                  ))
-                  : Icon(
-                Icons.person,
-                size: 24.sp,
-              ),
-            ),
-            Text(name),
-          ],
+              SizedBox(width: 10.w,),
+              Text(name),
+            ],
+          ),
         ),
-
-        Text(title),
-        Row(
-          children: [
-            Text(applied ? "참여중" : "대기중"),
-            Text("$people / $maxPeople")
-          ],
+        SizedBox(
+          width: 100.w,
+            child: Text(title)
+        ),
+        SizedBox(
+          width: 70.w,
+          child: Row(
+            children: [
+              Text(applied ? "참여중" : "대기중"),
+              Text("$people / $maxPeople")
+            ],
+          ),
         )
 
       ],
     );
   }
 }
+
+class _ImportantTravel extends StatelessWidget {
+  _ImportantTravel({super.key, required this.travelName});
+
+  List<String> travelName;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox();
+  }
+}
+
+class _Cost extends StatelessWidget {
+  _Cost({super.key, required this.cost});
+  int cost;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const Icon(Icons.attach_money),
+        Text("$cost")
+      ],
+    );
+  }
+}
+
+class _ApplyButton extends StatelessWidget {
+  const _ApplyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 30.h,
+      width: 100.w,
+      child: Center(child: Text("신청하기"),),
+    );
+  }
+}
+
+class _SettingButton extends StatelessWidget {
+  const _SettingButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(onPressed: (){}, icon: Icon(Icons.menu));
+  }
+}
+
 
