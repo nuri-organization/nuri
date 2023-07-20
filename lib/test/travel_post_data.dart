ce91a15e56db1816ff661520b3ce72f76a7531b0
jeavaljean <jeanvaljean0520@gmail.com>
2023-07-21 00:52:51 +0900

    [add] : 더미데이터 추가

--- lib/test/travel_post_data.dart
+++ lib/test/travel_post_data.dart
@@ -13,7 +13,7 @@ class TestTravelPostData{
         userProfile: "https://media.licdn.com/dms/image/C5603AQEhKKAy1Ij9xQ/profile-displayphoto-shrink_100_100/0/1639652531388?e=1695254400&v=beta&t=DY39uRxe6FR_IuJlFVJ5axN2D3x3O3rYOdqF46GKo74"
       ),
       mainTravel: ["칼바람, 우르프"],
-      cost: 0,
+      cost: 30000,
       startDate: "23/1/1",
       endDate: "24/3/3",
       currentPeople: 2,
@@ -29,13 +29,81 @@ class TestTravelPostData{
             userName: "moon",
             userProfile: "https://media.licdn.com/dms/image/D5635AQF1wkD91hA9ig/profile-framedphoto-shrink_100_100/0/1681437828406?e=1690452000&v=beta&t=_gFnvXJfMotP6-b3DhN-XzRfYJ1yC4LxXgpwSKfuAeQ"
         ),
-        mainTravel: ["칼바람, 우르프"],
-        cost: 0,
+        mainTravel: ["칼바람", "urf","협곡" , "랭크"],
+        cost: 1100,
         startDate: "23/1/1",
         endDate: "24/3/3",
         currentPeople: 2,
         maxPeople: 5,
         applied: true
-    )
+    ),
+    const TravelPostModel(
+        travelId: 3,
+        title: "소환사의 협곡",
+        content: "소환사의 협곡에 가실 용사분들 5명 구합니다,",
+        userInfo: UserInfoModel(
+            userId: 0,
+            userName: "hamilton",
+            userProfile: "https://media.licdn.com/dms/image/C5603AQEhKKAy1Ij9xQ/profile-displayphoto-shrink_100_100/0/1639652531388?e=1695254400&v=beta&t=DY39uRxe6FR_IuJlFVJ5axN2D3x3O3rYOdqF46GKo74"
+        ),
+        mainTravel: ["칼바람, 우르프"],
+        cost: 30000,
+        startDate: "23/1/1",
+        endDate: "24/3/3",
+        currentPeople: 2,
+        maxPeople: 5,
+        applied: true
+    ),
+    const TravelPostModel(
+        travelId: 1,
+        title: "소환사의 협곡",
+        content: "소환사의 협곡에 가실 용사분들 5명 구합니다,",
+        userInfo: UserInfoModel(
+            userId: 0,
+            userName: "hamilton",
+            userProfile: "https://media.licdn.com/dms/image/C5603AQEhKKAy1Ij9xQ/profile-displayphoto-shrink_100_100/0/1639652531388?e=1695254400&v=beta&t=DY39uRxe6FR_IuJlFVJ5axN2D3x3O3rYOdqF46GKo74"
+        ),
+        mainTravel: ["칼바람, 우르프"],
+        cost: 30000,
+        startDate: "23/1/1",
+        endDate: "24/3/3",
+        currentPeople: 2,
+        maxPeople: 5,
+        applied: true
+    ),
+    const TravelPostModel(
+        travelId: 1,
+        title: "소환사의 협곡",
+        content: "소환사의 협곡에 가실 용사분들 5명 구합니다,",
+        userInfo: UserInfoModel(
+            userId: 0,
+            userName: "hamilton",
+            userProfile: "https://media.licdn.com/dms/image/C5603AQEhKKAy1Ij9xQ/profile-displayphoto-shrink_100_100/0/1639652531388?e=1695254400&v=beta&t=DY39uRxe6FR_IuJlFVJ5axN2D3x3O3rYOdqF46GKo74"
+        ),
+        mainTravel: ["칼바람, 우르프"],
+        cost: 30000,
+        startDate: "23/1/1",
+        endDate: "24/3/3",
+        currentPeople: 2,
+        maxPeople: 5,
+        applied: true
+    ),
+    const TravelPostModel(
+        travelId: 1,
+        title: "소환사의 협곡",
+        content: "소환사의 협곡에 가실 용사분들 5명 구합니다,",
+        userInfo: UserInfoModel(
+            userId: 0,
+            userName: "hamilton",
+            userProfile: "https://media.licdn.com/dms/image/C5603AQEhKKAy1Ij9xQ/profile-displayphoto-shrink_100_100/0/1639652531388?e=1695254400&v=beta&t=DY39uRxe6FR_IuJlFVJ5axN2D3x3O3rYOdqF46GKo74"
+        ),
+        mainTravel: ["칼바람, 우르프"],
+        cost: 30000,
+        startDate: "23/1/1",
+        endDate: "24/3/3",
+        currentPeople: 2,
+        maxPeople: 5,
+        applied: true
+    ),
   ];
 }
