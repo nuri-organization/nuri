1dacd2b6fab1079e6d992ff4cead8446a3917524
jeavaljean <jeanvaljean0520@gmail.com>
2023-07-24 02:14:25 +0900

    [add] : 로그인 ui 로직 추가

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nuri/config/enum.dart';
 import 'package:nuri/config/scaffold.dart';
+import 'package:nuri/cubit/Login/login_cubit.dart';
+import 'package:nuri/ui/screen/login/login_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +16,18 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return NuriScaffold(isInBottomNavigation: true,);
+    return BlocBuilder<LoginCubit, LoginState>(
+      builder: (context, state) {
+        if(state.loginStatus == LoginStatus.finish) {
+          return NuriScaffold(
+            isInBottomNavigation: true,
+          );
+        }
+        if(state.loginStatus == LoginStatus.loading) {
+          return Container();
+        }
+        return NuriScaffold(isInAppbar: false, child: LoginScreen());
+      },
+    );
   }
 }
