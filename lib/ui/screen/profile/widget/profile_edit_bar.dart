f36cbec188921c6d2ce7bc4ad6688a9bba97cb1f
jeavaljean <jeanvaljean0520@gmail.com>
2023-07-26 01:22:53 +0900

    [add] : 프로필 수정 페이지 라우트

--- lib/ui/screen/profile/widget/profile_edit_bar.dart
+++ lib/ui/screen/profile/widget/profile_edit_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nuri/ui/screen/profile/profile_fetch_screen.dart';
 
 class ProfileEditBar extends StatelessWidget {
   const ProfileEditBar({super.key});
@@ -20,7 +21,9 @@ class _ProfileEditButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){},
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => const ProfileFetchScreen()));
+      },
       child: Container(
         height: 25.h,
         width: 100.w,
