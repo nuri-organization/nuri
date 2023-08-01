ee7ed07e70c6cea115b63ac8983e039cac85a584
jeavaljean <jeanvaljean0520@gmail.com>
2023-08-01 18:49:05 +0900

    [add] : travel accept bloc-cubit 추가

---
+++ lib/cubit/travel/accept/travel_accept_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:nuri/repository/travel_accept_repository.dart';
+
+part 'travel_accept_state.dart';
+
+class TravelAcceptCubit extends Cubit<TravelAcceptState> {
+  TravelAcceptCubit(this.travelAcceptRepository) : super(TravelAcceptState());
+
+  final TravelAcceptRepository travelAcceptRepository;
+
+  void userAccept({required String userId, required bool respond, required int travelId}) async{
+    var result = await travelAcceptRepository.userAccept(userId: userId, respond: respond, travelId: travelId);
+
+  }
+
+}
---
+++ lib/cubit/travel/accept/travel_accept_state.dart
@@ -0,0 +1,7 @@
+part of 'travel_accept_cubit.dart';
+
+class TravelAcceptState extends Equatable {
+
+  @override
+  List<Object> get props => [];
+}
