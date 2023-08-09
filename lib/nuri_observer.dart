ab481b4c0fa8096958e114d216fd277b8015b52d
jeavaljean <jeanvaljean0520@gmail.com>
2023-08-09 18:30:11 +0900

    fix : pub vertion

--- lib/nuri_observer.dart
+++ lib/nuri_observer.dart
@@ -1,11 +1,11 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 class NuriObserver extends BlocObserver{
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
 
-    debugPrint("${bloc.runtimeType} $change");
+    Logger().i("${bloc.runtimeType} $change");
   }
 }
